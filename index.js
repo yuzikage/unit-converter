b2ad9ec224ab79cf32b4f3db04a8470b5b86389c
Smit Sabalpara <130885468+yuzikage@users.noreply.github.com>
2026-05-05 22:53:45 +0530

    added DOM elements

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+// Get references to DOM elements
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("convert-btn");
+const lengthEl = document.getElementById("length-el");
+const volumeEl = document.getElementById("volume-el");
+const massEl = document.getElementById("mass-el");
+
